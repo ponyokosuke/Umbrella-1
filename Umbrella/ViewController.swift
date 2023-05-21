6cfa501d41df66e48862541fb0def0cfd2c61c36
KosukeYamashita <kosukeyamashitalit@gmail.com>
2023-05-21 15:51:54 +0900

    Initial Commit

---
+++ Umbrella/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Umbrella
+//
+//  Created by 山下幸助 on 2023/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
