19e75d9edfebfe33291889ea5921940b10254c5a
KosukeYamashita <kosukeyamashitalit@gmail.com>
2023-06-06 03:19:03 +0900

    新しいお天気APIの導入

--- Umbrella/SunnyViewController.swift
+++ Umbrella/SunnyViewController.swift
@@ -74,17 +74,30 @@ class SunnyViewController: UIViewController {
                 }
             }
             
-            getPopValue(apiKey: "fca09c676c26d6e1d67d6ac5fe12168d", latitude: latitude, longitude: longitude) { result in
+            fetchRainProbability(latitude: latitude, longitude: longitude) { result in
                 switch result {
-                case .success(let averagePop):
+                case .success(let rainPercentage):
                     DispatchQueue.main.async {
-                        self.rainypercentLabel.text = "\(averagePop)"
+                        self.rainypercentLabel.text = "\(rainPercentage)%"
                     }
-                    print("Rainy percentage: \(averagePop)%")
+                    print("降水確率: \(rainPercentage)%")
                 case .failure(let error):
-                    print("Error:", error.localizedDescription)
+                    print("エラー: \(error.localizedDescription)")
                 }
             }
+            
+            
+//            getPopValue(apiKey: "fca09c676c26d6e1d67d6ac5fe12168d", latitude: latitude, longitude: longitude) { result in
+//                switch result {
+//                case .success(let averagePop):
+//                    DispatchQueue.main.async {
+//                        self.rainypercentLabel.text = "\(averagePop)"
+//                    }
+//                    print("Rainy percentage: \(averagePop)%")
+//                case .failure(let error):
+//                    print("Error:", error.localizedDescription)
+//                }
+//            }
             
             fetchCityFromCoordinates(latitude: latitude, longitude: longitude) { result in
                 switch result {
@@ -193,15 +206,8 @@ class SunnyViewController: UIViewController {
         }
     }
     
-    func getPopValue(apiKey: String, latitude: Double, longitude: Double, completion: @escaping (Result<Double, Error>) -> Void) {
-        let baseURL = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
-        let urlString = "\(baseURL)?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)"
-        
-        guard let url = URL(string: urlString) else {
-            let error = NSError(domain: "Invalid URL", code: 0, userInfo: nil)
-            completion(.failure(error))
-            return
-        }
+    func fetchRainProbability(latitude: Double, longitude: Double, completion: @escaping (Result<Int, Error>) -> Void) {
+        let url = URL(string: "http://weather.livedoor.com/forecast/webservice/json/v1?city=\(latitude),\(longitude)")!
         
         let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
             if let error = error {
@@ -209,45 +215,92 @@ class SunnyViewController: UIViewController {
                 return
             }
             
-            guard let httpResponse = response as? HTTPURLResponse,
-                  (200...299).contains(httpResponse.statusCode) else {
-                let error = NSError(domain: "Invalid response", code: 0, userInfo: nil)
+            guard let data = data else {
+                let error = NSError(domain: "WeatherAPIError", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid data received"])
                 completion(.failure(error))
                 return
             }
             
-            if let data = data {
-                do {
-                    let json = try JSONSerialization.jsonObject(with: data, options: [])
-                    print(json)
-                    if let forecastData = json as? [String: Any],
-                       let hourlyData = forecastData["hourly"] as? [[String: Any]] {
-                        
-                        var totalPop = 0.0
-                        var totalCount = 0
-                        
-                        for hour in hourlyData {
-                            if let pop = hour["pop"] as? Double {
-                                totalPop += pop
-                                totalCount += 1
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
+                
+                if let forecasts = json["forecasts"] as? [[String: Any]], let todayForecast = forecasts.first {
+                    if let telop = todayForecast["telop"] as? String {
+                        if telop.contains("雨") {
+                            if let chanceOfRain = todayForecast["chanceOfRain"] as? [String: Any], let rainPercentage = chanceOfRain["T06_12"] as? Int {
+                                completion(.success(rainPercentage))
+                                return
                             }
                         }
-                        
-                        let averagePop = totalPop / Double(totalCount)
-                        completion(.success(averagePop))
-                        
-                    } else {
-                        let error = NSError(domain: "Forecast data not available", code: 0, userInfo: nil)
-                        completion(.failure(error))
                     }
-                } catch {
-                    completion(.failure(error))
                 }
+                
+                let error = NSError(domain: "WeatherAPIError", code: 0, userInfo: [NSLocalizedDescriptionKey: "No rain data available"])
+                completion(.failure(error))
+            } catch {
+                completion(.failure(error))
             }
         }
         
         task.resume()
     }
+
+    
+//    func getPopValue(apiKey: String, latitude: Double, longitude: Double, completion: @escaping (Result<Double, Error>) -> Void) {
+//        let baseURL = "https://pro.openweathermap.org/data/2.5/forecast/hourly"
+//        let urlString = "\(baseURL)?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)"
+//
+//        guard let url = URL(string: urlString) else {
+//            let error = NSError(domain: "Invalid URL", code: 0, userInfo: nil)
+//            completion(.failure(error))
+//            return
+//        }
+//
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            if let error = error {
+//                completion(.failure(error))
+//                return
+//            }
+//
+//            guard let httpResponse = response as? HTTPURLResponse,
+//                  (200...299).contains(httpResponse.statusCode) else {
+//                let error = NSError(domain: "Invalid response", code: 0, userInfo: nil)
+//                completion(.failure(error))
+//                return
+//            }
+//
+//            if let data = data {
+//                do {
+//                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+//                    print(json)
+//                    if let forecastData = json as? [String: Any],
+//                       let hourlyData = forecastData["hourly"] as? [[String: Any]] {
+//
+//                        var totalPop = 0.0
+//                        var totalCount = 0
+//
+//                        for hour in hourlyData {
+//                            if let pop = hour["pop"] as? Double {
+//                                totalPop += pop
+//                                totalCount += 1
+//                            }
+//                        }
+//
+//                        let averagePop = totalPop / Double(totalCount)
+//                        completion(.success(averagePop))
+//
+//                    } else {
+//                        let error = NSError(domain: "Forecast data not available", code: 0, userInfo: nil)
+//                        completion(.failure(error))
+//                    }
+//                } catch {
+//                    completion(.failure(error))
+//                }
+//            }
+//        }
+//
+//        task.resume()
+//    }
 
     func fetchCityFromCoordinates(latitude: CLLocationDegrees, longitude: CLLocationDegrees, completion: @escaping (Result<String, Error>) -> Void) {
         let location = CLLocation(latitude: latitude, longitude: longitude)
