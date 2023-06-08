f1ea0834b5623a1f6601c793cb33cc850c1efa8f
KosukeYamashita <kosukeyamashitalit@gmail.com>
2023-06-09 07:03:57 +0900

    通知の許可要求コードの導入 通知の送信コードの導入 まだ、通知の内容は設定していません。

--- Umbrella/AppDelegate.swift
+++ Umbrella/AppDelegate.swift
@@ -1,23 +1,53 @@
-//
-//  AppDelegate.swift
-//  Umbrella
-//
-//  Created by 山下幸助 on 2023/05/21.
-//
-
 import UIKit
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            if granted {
+                // ユーザーが通知を許可した場合の処理を記述する
+                self.setupNotifications()
+            } else {
+                // ユーザーが通知を許可しなかった場合の処理を記述する
+            }
+        }
         return true
     }
     
-
+    // 通知のセットアップ
+    func setupNotifications() {
+        let center = UNUserNotificationCenter.current()
+        center.getNotificationSettings { (settings) in
+            if settings.authorizationStatus == .authorized {
+                // 通知の設定
+                self.scheduleMorningNotification()
+            }
+        }
+    }
+    
+    // 毎朝7時に通知をスケジュールする
+    func scheduleMorningNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "おはようございます"
+        content.body = "今日も素敵な1日をお過ごしください"
+        
+        var dateComponents = DateComponents()
+        dateComponents.hour = 7
+        dateComponents.minute = 0
+        
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: true)
+        let request = UNNotificationRequest(identifier: "MorningNotification", content: content, trigger: trigger)
+        
+        let center = UNUserNotificationCenter.current()
+        center.add(request) { (error) in
+            if let error = error {
+                print("通知のスケジュールに失敗しました: \(error.localizedDescription)")
+            }
+        }
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
@@ -32,6 +62,42 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
+    // ... 他のAppDelegateのメソッド
 
 }
 
+
+//import UIKit
+//import UserNotifications
+//
+//@main
+//class AppDelegate: UIResponder, UIApplicationDelegate {
+//
+//    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+//        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+//            if granted {
+//                // ユーザーが通知を許可した場合の処理を記述する
+//            } else {
+//                // ユーザーが通知を許可しなかった場合の処理を記述する
+//            }
+//        }
+//        return true
+//    }
+//
+//    // MARK: UISceneSession Lifecycle
+//
+//    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+//        // Called when a new scene session is being created.
+//        // Use this method to select a configuration to create the new scene with.
+//        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+//    }
+//
+//    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+//        // Called when the user discards a scene session.
+//        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
+//        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+//    }
+//
+//    // ... 他のAppDelegateのメソッド
+//
+//}
