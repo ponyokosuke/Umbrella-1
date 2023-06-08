b2bbee7ed56ec0ba1cd0d48ade24a64c0cdd35fc
KosukeYamashita <kosukeyamashitalit@gmail.com>
2023-06-08 21:16:01 +0900

    画面が真っ黒 Table Viewの追加

---
+++ Umbrella/WeatherTableViewCell.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var datelistLabel: UILabel!
+    
+    @IBOutlet weak var popLabel: UILabel!
+    
+    @IBOutlet weak var weatherImage: UIImageView!
+}
