e1865d062237f834b8d914d74a55314c351d8a1a
KosukeYamashita <kosukeyamashitalit@gmail.com>
2023-06-11 06:59:12 +0900

    機能完成！！！！！！

--- Umbrella/WeatherForecastViewController.swift
+++ Umbrella/WeatherForecastViewController.swift
@@ -1,21 +1,56 @@
 import UIKit
 
 class WeatherForecastViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    
+
     @IBOutlet weak var tableView: UITableView!
-    
+
+    var maxPops: [Double] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         tableView.register(UINib(nibName: "WeatherTableViewCell", bundle: nil), forCellReuseIdentifier: "WeatherCell")
         tableView.dataSource = self
         tableView.reloadData()
-        tableView.register(WeatherTableViewCell.self, forCellReuseIdentifier: "WeatherCell")
 
+        let testDate = Date()
+
+        if let latitude = UserDefaults.standard.value(forKey: "latitude") as? Double,
+           let longitude = UserDefaults.standard.value(forKey: "longitude") as? Double {
+            // APIキーの設定
+            let apiKey = "fca09c676c26d6e1d67d6ac5fe12168d"
+
+            getDTandPopValues(for: testDate, apiKey: apiKey, latitude: latitude, longitude: longitude) { [weak self] result in
+                guard let self = self else {
+                    return
+                }
+
+                switch result {
+                case .success(let dtPopValues):
+                    // 取得した配列を出力
+                    for (dt, pop) in dtPopValues {
+                        print("dt: \(dt), pop: \(pop)")
+                    }
+
+                    self.maxPops = self.findMaxPops(dtPopValues: dtPopValues)
+
+                    DispatchQueue.main.async {
+                        self.tableView.reloadData()
+                    }
+
+                case .failure(let error):
+                    print("Error: \(error)")
+                    // エラー処理を行う
+                }
+            }
+        } else {
+            print("Latitude or longitude not found in UserDefaults.")
+            // エラー処理を行う
+        }
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return maxPops.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -28,29 +63,25 @@ class WeatherForecastViewController: UIViewController, UITableViewDataSource, UI
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "MM/dd"
             let dateString = dateFormatter.string(from: futureDate)
-            //cell.datelistLabel.text = dateString // 日付をdatelistLabelに表示する
             cell.datelistLabel?.text = dateString
+        }
 
-            if let latitude = UserDefaults.standard.value(forKey: "latitude") as? Double,
-               let longitude = UserDefaults.standard.value(forKey: "longitude") as? Double {
-                getMaxPopValue(for: futureDate, apiKey: "YOUR_API_KEY", latitude: latitude, longitude: longitude) { result in
-                    switch result {
-                    case .success(let maxPop):
-                        DispatchQueue.main.async {
-                            cell.poplistLabel.text = "\(maxPop)" // 最大の"pop"をpoplistLabelに表示する
-                        }
-                    case .failure(let error):
-                        print("Error: \(error)")
-                        // エラー処理を行う
-                    }
-                }
-            }
+        let maxPop = maxPops[indexPath.row]
+        let formattedPop = String(format: "%.0f%%", maxPop * 100) // パーセンテージに変換
+        cell.poplistLabel?.text = formattedPop
+
+        if maxPop <= 0.3 {
+            cell.imagelistLabel?.image = UIImage(named: "sunnyumbrella")
+        } else if maxPop <= 0.7 {
+            cell.imagelistLabel?.image = UIImage(named: "cloudyumbrella")
+        } else {
+            cell.imagelistLabel?.image = UIImage(named: "rainyumbrella")
         }
 
         return cell
     }
 
-    func getMaxPopValue(for date: Date, apiKey: String, latitude: Double, longitude: Double, completion: @escaping (Result<Double, Error>) -> Void) {
+    func getDTandPopValues(for date: Date, apiKey: String, latitude: Double, longitude: Double, completion: @escaping (Result<[(String, Double)], Error>) -> Void) {
         let baseURL = "https://api.openweathermap.org/data/2.5/forecast"
         let urlString = "\(baseURL)?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
 
@@ -80,20 +111,20 @@ class WeatherForecastViewController: UIViewController, UITableViewDataSource, UI
                         let formatter = DateFormatter()
                         formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 
-                        var maxPop = 0.0
+                        var dtPopValues: [(String, Double)] = []
 
                         for hour in hourlyData {
                             if let dateString = hour["dt_txt"] as? String, let pop = hour["pop"] as? Double {
                                 if let date = formatter.date(from: dateString), Calendar.current.isDate(date, inSameDayAs: date) {
-                                    maxPop = max(maxPop, pop)
+                                    dtPopValues.append((dateString, pop))
                                 }
                             }
                         }
 
-                        if maxPop > 0 {
-                            completion(.success(maxPop))
+                        if dtPopValues.count > 0 {
+                            completion(.success(dtPopValues))
                         } else {
-                            let error = NSError(domain: "No pop data available for the specified date", code: 0, userInfo: nil)
+                            let error = NSError(domain: "No dt_txt and pop data available for the specified date", code: 0, userInfo: nil)
                             completion(.failure(error))
                         }
                     } else {
@@ -107,5 +138,18 @@ class WeatherForecastViewController: UIViewController, UITableViewDataSource, UI
         }
 
         task.resume()
+    }
+
+    func findMaxPops(dtPopValues: [(String, Double)]) -> [Double] {
+        var maxPops: [Double] = []
+
+        for i in stride(from: 0, to: dtPopValues.count, by: 8) {
+            let endIndex = min(i + 8, dtPopValues.count)
+            let subArray = dtPopValues[i..<endIndex]
+            let maxPop = subArray.max(by: { $0.1 < $1.1 })?.1 ?? 0.0
+            maxPops.append(maxPop)
+        }
+
+        return maxPops
     }
 }
